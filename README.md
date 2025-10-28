29a58fb074883e115a5053ad618f1de47ea1a7fa
HENDRIX-ZT2 <HENDRIX-ZT2@users.noreply.github.com>
2025-10-28 12:26:59 +0100

    Rewrite with CLI

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 Convert JPOG's PPM files to TGA and vice versa.
 
 # Installation
-- Install python 3.6 and run `pip install numpy` if you haven't already done so.
+- Install python 3.6+ and run `pip install numpy` if you haven't already done so.
 - Download this script and unzip anywhere.
 
 # How To Use
-- See comments at the end of the script.
+Call it from a CMD or PowerShell window like this:
+
+`python convert-ppm.py [FOLDER] [OUTPUT]`
+
+`python convert-ppm.py C:\Users\arnfi\Desktop\Coding\JPOG .tga`
--- convert-ppm.py
+++ convert-ppm.py
@@ -1,84 +1,78 @@
+import argparse
+import logging
 import os
 import numpy as np
 from struct import pack, unpack
 
-def read_tga(name):
-	tga_name = name[:-4]+".tga"
-	print("\nReading",os.path.basename(tga_name))
-	with open(tga_name, 'rb') as f:
-		datastream = f.read()
-	a, b, c, d, e, x_ori, y_ori, WIDTH, HEIGHT, f, g = unpack("3B H B 4H 2B", datastream[0:18])
-	data = np.frombuffer(datastream[18:], dtype=np.uint8)
-	data = np.reshape(data, (HEIGHT, WIDTH, 4))
-	out = np.zeros((HEIGHT, WIDTH, 4), dtype=np.uint8)
-	for x in range(0, HEIGHT):
-		for y in range(0, WIDTH):
-			r, g, b, a = data[x,y]
-			out[x,y] = [b, g, r, a]
-	return np.flipud(out)
-	
-def write_tga(name, data):
-	tga_name = name[:-4]+".tga"
-	print("\nWriting",os.path.basename(tga_name))
-	HEIGHT,WIDTH, col_i = data.shape
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def ensure_ext(filepath, ext):
+	return f"{os.path.splitext(filepath)[0]}{ext}"
+
+
+def read_image(height, stream, width):
+	data = np.empty((height, width, 4), dtype=np.uint8)
+	stream.readinto(data)
+	return data
+
+
+def read_tga(filepath):
+	logging.info(f"Reading {os.path.basename(filepath)}")
+	with open(filepath, 'rb') as stream:
+		a, b, c, d, e, x_ori, y_ori, width, height, f, g = unpack("3B H B 4H 2B", stream.read(18))
+		data = read_image(height, stream, width)
+	return data[::-1, :, (2, 1, 0, 3)]
+
+
+def write_tga(filepath, data):
+	filepath = ensure_ext(filepath, ".tga")
+	logging.info(f"Writing {os.path.basename(filepath)}")
+	HEIGHT, WIDTH, col_i = data.shape
 	x_ori = 0
 	y_ori = HEIGHT
 	header = pack("3B H B 4H 2B", 0, 0, 2, 0, 0, x_ori, y_ori, WIDTH, HEIGHT, 32, 8)
-	out = np.zeros((HEIGHT, WIDTH, 4), dtype=np.uint8)
-	for x in range(0, HEIGHT):
-		for y in range(0, WIDTH):
-			r, g, b, a = data[x,y]
-			out[x,y] = [b, g, r, a]
-	bbytes = np.flipud(out).tobytes()
-	with open(tga_name, 'wb') as f:
-		f.write(b"".join( (header, bbytes)) )
-	print(x,y,col_i)
-	
-def read_ppm(name):
-	print("\nReading",os.path.basename(name))
-	with open(name, 'rb') as f:
-		datastream = f.read()
-	magic, comment, dim, nc, rest = datastream[0:100].split(b"\n")
-	x, y = dim.split(b" ")
-	WIDTH = int(x)
-	HEIGHT = int(y)
-	MAXVAL = int(nc)
-	header = b"\n".join( (magic, comment, dim, nc ) )
-	start = len(header)+1
-	data = np.frombuffer(datastream[start:start+(WIDTH*HEIGHT*4)], dtype=np.uint8)
-	data = np.reshape(data, (HEIGHT, WIDTH, 4))
-	return data
-	
-def write_ppm(name, data):
-	ppm_name = name[:-4]+".ppm"
-	print("\nWriting",os.path.basename(ppm_name))
-	HEIGHT,WIDTH, col_i = data.shape
-	with open(ppm_name, 'wb') as f:
-		f.write( b"\n".join( (b"P7", b"# File created by Blue Tongue Software's PPM Photoshop format plugin.", (str(WIDTH)+" "+str(HEIGHT)).encode(), b"255", data.tobytes()) ) )
+	with open(filepath, 'wb') as f:
+		f.write(header)
+		f.write(data[::-1, :, (2, 1, 0, 3)].tobytes())
 
-def ppm_2_tga(name):
-	data = read_ppm(name)
-	write_tga(name, data)
-	
-def tga_2_ppm(name):
-	data = read_tga(name)
-	write_ppm(name, data)
 
-# def show_data(data):
-	# from matplotlib import pyplot as plt
-	# plt.imshow(data2, interpolation='nearest')
-	# plt.show()
+def read_ppm(filepath):
+	logging.info(f"Reading {os.path.basename(filepath)}")
+	with open(filepath, 'rb') as stream:
+		magic, comment, dim, nc, rest = stream.read(100).split(b"\n")
+		x, y = dim.split(b" ")
+		width = int(x)
+		height = int(y)
+		maxval = int(nc)
+		header = b"\n".join( (magic, comment, dim, nc ) )
+		stream.seek(len(header)+1)
+		return read_image(height, stream, width)
 
-dir = "C:/Program Files (x86)/Universal Interactive/Blue Tongue Software/Jurassic Park Operation Genesis/JPOG/Data/Particle"
 
-#convert PPM to TGA
-for f in os.listdir(dir):
-	if f.lower().endswith(".ppm"):
-		filepath = os.path.join(dir, f)
-		ppm_2_tga( filepath )
-		
-# #convert TGA to PPM
-# for f in os.listdir(dir):
-	# if f.lower().endswith(".tga"):
-		# filepath = os.path.join(dir, f)
-		# tga_2_ppm( filepath )
+def write_ppm(filepath, data):
+	filepath = ensure_ext(filepath, ".ppm")
+	logging.info(f"Writing {os.path.basename(filepath)}")
+	height, width, col_i = data.shape
+	with open(filepath, 'wb') as f:
+		f.write( b"\n".join( (b"P7", b"# File created by Blue Tongue Software's PPM Photoshop format plugin.", f"{width} {height}".encode(), b"255", data.tobytes()) ) )
+
+
+def resize(folder, output=".ppm"):
+	for name in os.listdir(folder):
+		filepath = os.path.join(folder, name)
+		if "ppm" in output.lower():
+			if name.lower().endswith(".tga"):
+				write_ppm(filepath, read_tga(filepath))
+		else:
+			if name.lower().endswith(".ppm"):
+				write_tga(filepath, read_ppm(filepath))
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(prog='convert-ppm')
+	parser.add_argument('dir', nargs='?', help='Folder containing all .ppm or .tga files')
+	parser.add_argument('output', nargs='?', help='Output format - .ppm or .tga')
+	args = parser.parse_args()
+	resize(args.dir, args.output)
